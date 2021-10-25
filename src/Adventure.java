faa3ee1de0ad1fc8895190965c455bf821f2a62f
Hyttel <mikkel.hw@hotmail.com>
2021-10-25 14:44:08 +0200

    små rettelser

--- src/Adventure.java
+++ src/Adventure.java
@@ -7,18 +7,27 @@ public class Adventure {
     public void run() throws InterruptedException {
         boolean gameIsRunning = true;
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nWelcome to KEA!\n\nIn this game you play the character of an introverted student whose greatest fear is the social contact with other people.\nYour mission is to graduate school with the least amount of social interaction as possible. Type 'help' if you need guidance.\n\nGood luck!\n\n");
-        Thread.sleep(2000);
+        System.out.println("\nWelcome to KEA!\n\nIn this game you play the character of an introverted student whose " +
+            "greatest fear is the social contact with other people.\nYour mission is to graduate school with the least" +
+            " amount of social interaction as possible. Type 'help' if you need guidance.\n" +
+            "\nGood luck!");
+        Thread.sleep(1000);
+        System.out.println("...");
+        Thread.sleep(1000);
+        System.out.println("...");
+        Thread.sleep(1000);
+        System.out.println("...\n");
+
         player.setCurrentRoom(map.getRoom1());
         System.out.println(player.getCurrentRoomDescription());
 
         while (gameIsRunning) {
             String tekst = sc.nextLine().trim();
             if (tekst.equals("look")) {
-                System.out.println("Looking around");
+                System.out.println("Looking around...");
                 Thread.sleep(1000);
                 System.out.println("You are currently in " + player.getCurrentRoomName());
-                printItems();
+                player.lookForItem();
                 System.out.println();
             } else if (tekst.equals("exit")) {
                 System.out.println("You have exited");
@@ -29,32 +38,52 @@ public class Adventure {
                     "\nSouth: go south/ south/ s" +
                     "\nEast: go east/ east/ e" +
                     "\nWest: go west/ west/ w" +
-                    "\n\nTry one of them out now!");
-            } else if ((tekst.equalsIgnoreCase("east") || tekst.equalsIgnoreCase("e") || tekst.equalsIgnoreCase("go east")) && player.getEastRoom() != null) {
+                    "\n\nTry one of them out now!\n" +
+                    "\nTo pick something up type: 'take' + 'name of the item'" +
+                    "\nTo drop the item again type: 'drop' + 'name of the item'" +
+                    "\nType 'inventory' to see all your items");
+            } else if ((tekst.equalsIgnoreCase("east")
+                || tekst.equalsIgnoreCase("e")
+                || tekst.equalsIgnoreCase("go east"))
+                && player.getEastRoom() != null) {
                 // go east
                     walking("east");
                     player.goEast();
-                    System.out.println("Entering " + player.getCurrentRoomName() + ". " + player.getCurrentRoomDescription());
-                } else if ((tekst.equalsIgnoreCase("south") || tekst.equalsIgnoreCase("s") || tekst.equalsIgnoreCase("go south")) && player.getSouthRoom() != null) {
+                    System.out.println("Entering " + player.getCurrentRoomName() + ". "
+                        + player.getCurrentRoomDescription());
+                } else if ((tekst.equalsIgnoreCase("south")
+                || tekst.equalsIgnoreCase("s")
+                || tekst.equalsIgnoreCase("go south"))
+                && player.getSouthRoom() != null) {
                 // go south
                     walking("south");
                     player.goSouth();
-                    System.out.println("Entering " + player.getCurrentRoomName() + ". " + player.getCurrentRoomDescription());
+                    System.out.println("Entering " + player.getCurrentRoomName() + ". "
+                        + player.getCurrentRoomDescription());
                 }
-              else if ((tekst.equalsIgnoreCase("west") || tekst.equalsIgnoreCase("w") || tekst.equalsIgnoreCase("go west"))&& player.getWestRoom() != null) {
+              else if ((tekst.equalsIgnoreCase("west")
+                || tekst.equalsIgnoreCase("w")
+                || tekst.equalsIgnoreCase("go west"))
+                && player.getWestRoom() != null) {
                 // go west
                     walking("west");
                     player.goWest();
-                    System.out.println("Entering " + player.getCurrentRoomName() + ". " + player.getCurrentRoomDescription());
+                    System.out.println("Entering " + player.getCurrentRoomName() + ". "
+                        + player.getCurrentRoomDescription());
 
-             } else if ((tekst.equalsIgnoreCase("north") || tekst.equalsIgnoreCase("n") || tekst.equalsIgnoreCase("go north"))&& player.getNorthRoom() != null) {
+             } else if ((tekst.equalsIgnoreCase("north")
+                || tekst.equalsIgnoreCase("n")
+                || tekst.equalsIgnoreCase("go north"))
+                && player.getNorthRoom() != null) {
                 // go north
                     walking("north");
                     player.goNorth();
-                    System.out.println("Entering " + player.getCurrentRoomName() + ". " + player.getCurrentRoomDescription());
+                    System.out.println("Entering " + player.getCurrentRoomName() + ". "
+                        + player.getCurrentRoomDescription());
             }else if(tekst.contains("take")){
                 tekst = tekst.substring(5);
-                takeItem(tekst);
+                player.takeTheItem(tekst);
+                System.out.println("You have picked up " + player.getItems() + " from the ground.");
             }else if(tekst.equals("inventory")){
                 System.out.print("Your inventory: ");
                 for (int i = 0; i < player.getItems().size(); i++) {
@@ -63,8 +92,9 @@ public class Adventure {
                 System.out.println();
             }else if(tekst.contains("drop")){
                 tekst = tekst.substring(5);
-                takeItem(tekst);
-                dropItem(tekst);
+                player.takeTheItem(tekst);
+                player.dropItem(tekst);
+                System.out.println("You dropped " +  player.getItems() + " on the ground");
             }
               else{
                 System.out.println("You search this way, but unfortunately it goes directly into a wall. Try another way!");
@@ -76,29 +106,4 @@ public class Adventure {
         System.out.println("Leaving " + player.getCurrentRoomName() + " and walking " + direction + "...");
         Thread.sleep(1000);
     }
-
-    //kan gøres pænere
-    public void printItems(){
-        System.out.print("You take a quick look around and find: ");
-        for (int i = 0; i < player.getCurrentRoom().getItems().size(); i++) {
-            System.out.print(player.getCurrentRoom().getItems().get(i) + " ");
-        }
-    }
-    public void takeItem(String tekst){
-        for (int i = 0; i < player.getCurrentRoom().getItems().size(); i++) {
-          if(player.getCurrentRoom().getItems().get(i).getName().equals(tekst)){
-              player.takeItem(player.getCurrentRoom().getItems().get(i));
-              player.getCurrentRoom().getItems().remove(i);
-          }
-        }
-    }
-    public void dropItem(String tekst){
-        for (int i = 0; i < player.getItems().size(); i++) {
-            if(player.getItems().get(i).getName().equals(tekst)){
-                player.getCurrentRoom().getItems().add(player.getItems().get(i));
-                player.getItems().remove(i);
-            }
-        }
-    }
-
 }
--- src/Player.java
+++ src/Player.java
@@ -4,26 +4,24 @@ public class Player {
     private Room currentRoom;
     private ArrayList<Item> items = new ArrayList<>();
 
+
     public void setCurrentRoom(Room currentRoom) {
         this.currentRoom = currentRoom;
     }
-
-
-
     public void takeItem(Item item){
         items.add(item);
     }
+
     public Room getCurrentRoom() {
         return currentRoom;
     }
-
     public ArrayList<Item> getItems() {
         return items;
     }
-
     public String getCurrentRoomDescription(){
         return currentRoom.getDescription();
     }
+
     public void goEast(){
         currentRoom = currentRoom.getEast();
     }
@@ -50,5 +48,27 @@ public class Player {
     }
     public Room getEastRoom(){
         return currentRoom.getEast();
+    }
+    public void lookForItem(){
+        System.out.print("You take a quick look around and find: ");
+        for (int i = 0; i < getCurrentRoom().getItems().size(); i++) {
+            System.out.print(getCurrentRoom().getItems().get(i) + " ");
+        }
+    }
+    public void takeTheItem(String tekst){
+        for (int i = 0; i < getCurrentRoom().getItems().size(); i++) {
+            if(getCurrentRoom().getItems().get(i).getName().equals(tekst)){
+                takeItem(getCurrentRoom().getItems().get(i));
+                getCurrentRoom().getItems().remove(i);
+            }
+        }
+    }
+    public void dropItem(String tekst){
+        for (int i = 0; i < getItems().size(); i++) {
+            if(getItems().get(i).getName().equals(tekst)){
+                getCurrentRoom().getItems().add(getItems().get(i));
+                getItems().remove(i);
+            }
+        }
     }
 }
